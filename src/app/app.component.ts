cd0f3d9ccf9bde71d615a711dd001ccda35491c5
franco-extreme <franck_ral@hotmail.com>
2018-03-26 20:00:43 -0400

    ngMapsAppV1: Events in the map - Create markers, drag and get their coordinates.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MapsService } from './services/maps.service';
+
+import { Marker } from './interfaces/marker.interface';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +14,21 @@ export class AppComponent {
   lat: number = -17.370407;
   lng: number = -66.146319;
   zoom:number = 18;
+
+  constructor(private _mapService:MapsService) {
+
+  }
+
+  clickOnMap(event) {
+    let newMarker:Marker = {
+      lat: event.coords.lat,
+      lng: event.coords.lng,
+      title: "No title",
+      draggable: true
+    }
+    // console.log(event);
+
+    this._mapService.addNewMarker(newMarker);
+  }
 }
 //-17.370407, -66.146319
--- src/app/services/maps.service.ts
+++ src/app/services/maps.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Marker } from '../interfaces/marker.interface';
 
 @Injectable()
 export class MapsService {
 
-  constructor() { }
+  markers:Marker[] = [];
+
+  constructor() {
+    let newMarker:Marker = {
+      lat: -17.370407,
+      lng: -66.146319,
+      title: "Fidel Anze park",
+      draggable: true
+    };
+
+    this.markers.push(newMarker);
+  }
+
+  addNewMarker(marker: Marker) {
+    this.markers.push(marker);
+  }
 
 }
